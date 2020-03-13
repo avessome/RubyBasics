4e8ed9143815ce84c74492dc75297a0b356a1b03
avessome <bukethead@mail.ru>
2020-03-13 20:15:29 +0300

    Program is ready

--- Triangle.rb
+++ Triangle.rb
@@ -1,1 +1,8 @@
+puts "Введите основание треугольника:"
+base = gets.chomp.to_i
 
+puts "Введите высоту треугольника:"
+height = gets.chomp.to_i
+
+square = (base*height)/2
+puts "Площадь треугольника равна #{square}"
