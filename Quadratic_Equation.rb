3e05ce3cbb895b9129ef3ecc614f142a7df162e7
avessome <bukethead@mail.ru>
2020-03-14 11:50:02 +0300

    Program is ready

---
+++ Quadratic_Equation.rb
@@ -0,0 +1,22 @@
+puts "Введите коэфицент a:"
+a_coef = gets.chomp.to_i
+
+puts "Введите коэфицент b:"
+b_coef = gets.chomp.to_i
+
+puts "Введите коэфицент c:"
+c_coef = gets.chomp.to_i
+
+discriminant = b_coef**2-4*a_coef*c_coef
+
+if discriminant > 0
+  root = Math.sqrt (discriminant)
+  key1 = (-b_coef + root)/(2*a_coef)
+  key2 = (-b_coef - root)/(2*a_coef)
+  puts "Дискриминант равен: #{discriminant}, Первый корень равен: #{key1}, Второй корень равен: #{key2}."
+elsif discriminant == 0
+  key = (-b_coef)/(2*a_coef)
+  puts "Дискриминант равен: #{discriminant}, Корень равен: #{key}."
+else
+  puts "Дискриминант равен: #{discriminant}, поэтому корней нет."
+end
