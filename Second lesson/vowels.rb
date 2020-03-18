0b5b162535a330926049e348aecb4b352ebab54f
avessome <bukethead@mail.ru>
2020-03-18 12:54:27 +0300

    Код честно скоммуниздил и не полностью разобрался. По возможности, напишите, пожалуйста, что делает метод with_index(1)

--- Second lesson/vowels.rb
+++ Second lesson/vowels.rb
@@ -1,5 +1,9 @@
 hash = {}
+
 alphabet = ('a'..'z').to_a
+
 vowels = ["a", "e", "i", "o", "u", "y"]
+
 alphabet.each.with_index(1) { |letter, index| hash[letter] = index if vowels.include? letter }
+
 puts hash
