104989186df6e2c465405f4c441d748fd49eecc2
avessome <bukethead@mail.ru>
2020-03-18 12:50:57 +0300

    Program is ready

---
+++ Second lesson/vowels.rb
@@ -0,0 +1,5 @@
+hash = {}
+alphabet = ('a'..'z').to_a
+vowels = ["a", "e", "i", "o", "u", "y"]
+alphabet.each.with_index(1) { |letter, index| hash[letter] = index if vowels.include? letter }
+puts hash
